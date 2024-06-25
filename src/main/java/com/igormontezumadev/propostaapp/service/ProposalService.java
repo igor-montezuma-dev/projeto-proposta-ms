94f064a9b221f4c801c6e65090f8f82847f916c3
IgorMontezuma20 <igorminranda17@gmail.com>
2024-06-25 12:00:10 -0300

    feat: Add proposalÃ service

---
+++ src/main/java/com/igormontezumadev/propostaapp/service/ProposalService.java
@@ -0,0 +1,13 @@
+package com.igormontezumadev.propostaapp.service;
+
+import com.igormontezumadev.propostaapp.dto.ProposalRequestDTO;
+import com.igormontezumadev.propostaapp.dto.ProposalResponseDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProposalService {
+
+    public ProposalResponseDTO createProposal( ProposalRequestDTO requestDto) {
+        return null;
+    }
+}
